3a8323a00efc11f3316893872eaaf4e8b8d183ed
StevenMavuma <steven.mavuma@banquapp.com>
2026-04-23 09:07:53 +0200

    Refactor Program.cs to implement grade calculation and feedback

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade percentage: ");
+        float grade = float.Parse(Console.ReadLine());
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+        else
+        {
+            letter = "Grade not defined";
+        }
+
+
+        if (grade%10 >= 7)
+        {
+            letter ="+"+letter;
+        }
+        else if(grade%10 < 4)
+        {
+            letter ="-"+letter;
+        }
+
+        Console.WriteLine($"The letter grade for {grade}% is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations, you've passed the class!");
+        }
+        else
+        {
+            Console.WriteLine($"Unfortunately, you did not pass the class. Don't be discouraged, but try again next time.");
+        }
+
+
     }
 }
