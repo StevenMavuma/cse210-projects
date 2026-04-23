037b8ae4b34cf59e7b17001b4014d4643327bd10
StevenMavuma <steven.mavuma@banquapp.com>
2026-04-23 06:14:42 +0200

    Fix spacing in user prompts for first and last name

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,9 +4,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name?");
+        Console.Write("What is your first name? ");
         string fname = Console.ReadLine();
-        Console.Write("What is your last name?");
+        Console.Write("What is your last name? ");
         string lname = Console.ReadLine();
 
         Console.WriteLine($"Your name is {fname}, {lname}.");
